acace05dc5aa4db5e306b595f24b54376a61e66b
Daniel Chen <dychen@caltech.edu>
2012-04-27 18:44:11 -0700

    Added a Procfile, Changed HOST settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'NAME': 'emelody', # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'pCmgJ8vpCmgJ8v',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '/tmp',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
